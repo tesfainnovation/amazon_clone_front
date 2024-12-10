d996a93da0bbef2fdbeaf26c993780467fc0a0d3
tesfainnovation <tesfainnovation@gmail.com>
2024-12-09 19:53:22 -0600

    carousel module spelling err fixed

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,7 +1,7 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { img } from "./img/data.js";
-import classes from "./Carousel.Module.css";
+import classes from "./Carousel.module.css";
 
 function CarouselEffect() {
   return (
